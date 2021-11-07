df2f2a7473c996894174eb8158ecdc39b53559b3
Neeraj-Palliyali <palliyali.neeraj@gmail.com>
2021-11-07 21:12:56 +0530

    true dynamic

--- challenges/views.py
+++ challenges/views.py
@@ -1,22 +1,41 @@
 from django.http.request import HttpRequest
 from django.http.response import Http404, HttpResponseNotFound
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseRedirect
+
+from django.urls import reverse
+import challenges
+
+monthly_challenges = {
+    "january": "Eat no meat for a month",
+    "february": "Walk for an hour daily",
+    "march": "Learn Django for 3 hrs",
+    "april": "Learn Django for 3 hrs",
+    "may": "Walk for an hour daily",
+    "june": "Eat no meat for a month",
+    "july": "Learn Django for 3 hrs",
+    "august": "Walk for an hour daily",
+    "september": "Eat no meat for a month",
+    "october": "Learn Django for 3 hrs",
+    "november": "Walk for an hour daily",
+    "december": "Eat no meat for a month",
+}
 
 # Create your views here.
 
 
-def monthly_challenge(request, month):
-    if( month == 'january'):
-        challenge_text = "Eat no meat for a month"
-    
-    elif(month == "february"):
-        challenge_text = "Walk for an hour daily"
-    
-    elif(month == "march"):
-        challenge_text = "Learn Django for 3 hrs"
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
 
+    if(month > len(months)):
+        return HttpResponseNotFound("Invalid Month")
+    forward_month = months[month-1]
+    redirect_path = reverse("month-challenge", args=[forward_month])
+    return HttpResponseRedirect(redirect_path)
+
+
+def monthly_challenge(request, month):
+    if(month in monthly_challenges.keys()):
+        return HttpResponse(monthly_challenges[month])
     else:
         return HttpResponseNotFound("This month is not supported")
-
-    return HttpResponse(challenge_text)    
